9f171085e13f689105fe689c9e9b6d569ed1c54d
edwin <edwinbuscabus.amoy@gmail.com>
2021-09-20 12:16:46 +0800

    Initial Commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8"=>
+    <meta name="viewport" content="width=device-width", initial-scale=1.0">
+    <title>Prelim Exam</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+
+</head>
+<body>
+    <h1 align="Left"> 
+    <font face="Lato" color="White" size="10">Home</font> &nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <font face="cinzel" size="6">
+    
+    <style>
+        body{
+            background-image: url('/1.jpg');
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
+    </h1>
+        <br><br><br><br><br><br><br>
+        
+        </h1>
+        <h3 align="center">
+            <font face="Lato" color="White" size="6">
+                    Welcome to my simple Laravel Website
+            </font>
+        </h3>
+    <br>
+    <h3 align="center">
+    <a href="{{url('/profile')}}">Profile</a> 
+    <a href="{{url('/contact')}}">Contact</a>
+
+        </font>
+    </h3>
+
+</body>
+  
+</html>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8"=>
+    <meta name="viewport" content="width=device-width", initial-scale=1.0">
+    <title>Prelim Exam</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+
+</head>
+<body>
+    <h1 align="Left"> 
+    <font face="Lato" color="White" size="10">Profile</font> &nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <font face="cinzel" size="6">
+    
+    <style>
+        body{
+            background-image: url('/1.jpg');
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
+    </h1>
+        <br><br><br><br><br><br><br>
+        
+        </h1>
+        <h3 align="center">
+            <font face="Lato" color="White" size="6">
+                    Hi!! My Name is Edwin A. Buscabus. Taking up Bachelor in Science in Information Technology at Mater Dei College Tubigon, Bohol
+                    <br>
+
+            </font>
+        </h3>
+    <br>
+    <h3 align="center">
+    <a href="{{url('/home')}}">Home</a> 
+    <a href="{{url('/contact')}}">Contact</a>
+
+        </font>
+    </h3>
+
+</body>
+  
+</html>
